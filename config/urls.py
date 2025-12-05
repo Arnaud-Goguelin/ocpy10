aae13976c9695323d3ce3f35a6fbc283623b4940
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-12-05 11:42:25 +0100

    style: format file with ruff

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from authentication.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # auth
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
