36bd4580cb001aa9dcdcf66ac37c370027a8fbb5
Arnaud <124574198+Arnaud-Goguelin@users.noreply.github.com>
2025-12-25 19:08:34 +0100

    Feat/implement comments endpoints (#8)

--- issue/serializers.py
+++ issue/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Issue
+from .models import Comment, Issue
 
 
 class IssueSerializer(ModelSerializer):
     class Meta:
         model = Issue
         fields = ["title", "content", "status", "priority", "tags", "created_at", "project", "author", "assigned_users"]
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["title", "content", "author", "created_at"]
--- issue/views.py
+++ issue/views.py
@@ -1,8 +1,8 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Issue
-from .serializers import IssueSerializer
+from .models import Comment, Issue
+from .serializers import CommentSerializer, IssueSerializer
 
 
 @extend_schema_view(
@@ -34,4 +34,36 @@ from .serializers import IssueSerializer
 class IssueModelViewSet(ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
+    permission_classes = []
+
+
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get all Comments",
+        tags=["Comment"],
+    ),
+    retrieve=extend_schema(
+        summary="Get an Comment",
+        tags=["Comment"],
+    ),
+    create=extend_schema(
+        summary="Create an Comment",
+        tags=["Comment"],
+    ),
+    update=extend_schema(
+        summary="Update entirely an Comment",
+        tags=["Comment"],
+    ),
+    partial_update=extend_schema(
+        summary="Update one or many Comment's fields",
+        tags=["Comment"],
+    ),
+    destroy=extend_schema(
+        summary="Delete an Comment",
+        tags=["Comment"],
+    ),
+)
+class CommentModelViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
     permission_classes = []
