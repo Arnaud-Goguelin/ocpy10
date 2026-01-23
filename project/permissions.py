d1881eb0aecc4047eeddb21608abbf04cc613d55
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-01-23 18:44:38 +0100

    feat(project permissions): update IsContributor permissions into ContributorReadOnly + create WriteContributor permission to allow POST/PUT/PATCH/DELETE request under certains conditions

--- project/permissions.py
+++ project/permissions.py
@@ -1,20 +1,38 @@
 from rest_framework import permissions
 
-from .models import Project
 
-
-class IsContributor(permissions.BasePermission):
+class ContributorReadOnly(permissions.BasePermission):
     """
     Custom permission: only allows users to read Project, Issues, and Comments if they are contributors to the Project.
     """
 
-    def has_object_permission(self, request, view, obj):
-        # Only allow read operations
+    def has_permission(self, request, view):
+        # import ipdb
+        # ipdb.set_trace()
         if request.method not in permissions.SAFE_METHODS:
             return False
 
-        # define project depending of obj type (Project or Issue, Comment, etc.)
-        project = obj if isinstance(obj, Project) else view.project
+        project = getattr(view, "project", None)
+
+        if not project:
+            # No project found in view so nothing to check
+            return False
 
         # Check if user is a contributor of the project
-        return project.contributors.filter(id=request.user.id).exists()
+        return project.contributors.filter(id=request.user.id).exists() or project.author == request.user
+
+    # do not overwrite has_object_permission because contributors can only read objects they have access to
+
+
+class WriteContributor(permissions.BasePermission):
+    """
+    Custom permission: only allows users to create a contributor if they are the project author.
+    """
+
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return request.user == view.project.author
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == view.project.author
